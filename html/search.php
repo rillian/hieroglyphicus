23e4fea4d1a39adeb05b3f9cb6bd1a9fb6782e36
Ralph Giles <giles@thaumas.net>
2018-12-17 14:44:51 -0800

    Add a dummy query to search.php.
    Dump a table of all entries with 'R4' in the MdC column.
    This confirms the database is working and forms a basis
    for accepting search criteria from the client.

--- html/search.php
+++ html/search.php
@@ -7,3 +7,36 @@ try {
 } catch (PDOException $pe) {
   die("Could not connect to database $dbname: " . $pe->GetMessage());
 }
+
+$sql = 'SELECT * FROM entries
+  WHERE mdc LIKE ?';
+
+$query = $conn->prepare($sql);
+$query->execute(['R4']);
+$query->setFetchMode(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8>
+<title>Test search result</title>
+</head>
+<body>
+<h1>Hieroglyphicus</h1>
+<table>
+  <tr>
+    <th>MdC</th>
+    <th>transliteration</th>
+    <th>translation</th>
+  </tr>
+  <?php while ($entry = $query->fetch()): ?>
+  <tr>
+    <td><?php echo htmlspecialchars($entry['mdc']); ?></td>
+    <td><?php echo htmlspecialchars($entry['translitation']); ?></td>
+    <td><?php echo htmlspecialchars($entry['english']); ?></td>
+  </tr>
+  <?php endwhile; ?>
+</table>
+</body>
+</html>
