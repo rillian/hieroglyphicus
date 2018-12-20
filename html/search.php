cb37c103662923e6bd6be0184dc8d2ef1fc5b74e
Ralph Giles <giles@thaumas.net>
2018-12-19 22:20:48 -0800

    Remove successful db connection message.
    This was just to confirm it's working, and is redundant now
    that we show an query result.

--- html/search.php
+++ html/search.php
@@ -3,7 +3,6 @@ require_once 'dbconfig.php';
 
 try {
   $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-  echo "Connected to $host successfully.";
 } catch (PDOException $pe) {
   die("Could not connect to database $dbname: " . $pe->GetMessage());
 }
